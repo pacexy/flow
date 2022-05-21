862f7b52e84fef52d40cf90efbdeb794df7e251f
Saiki K <pacexy@gmail.com>
2022-05-21 15:38:36 +0800

    chore: gtm

--- apps/reader/src/pages/_document.tsx
+++ apps/reader/src/pages/_document.tsx
@@ -5,10 +5,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <GoogleTagManager />
         <link rel="icon" href="/icons/192.png"></link>
         <PWA />
       </Head>
       <body>
+        <GoogleTagManagerNoScript />
         <Main />
         <NextScript />
         <PreventFlash />
@@ -33,5 +35,39 @@ function PWA() {
       />
       <link rel="apple-touch-icon" href="/icons/192.png" />
     </>
+  )
+}
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
+
+function GoogleTagManager() {
+  if (!GTM_ID) return null
+  return (
+    // eslint-disable-next-line @next/next/next-script-for-ga
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+      }}
+    />
+  )
+}
+
+function GoogleTagManagerNoScript() {
+  if (!GTM_ID) return null
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
   )
 }
