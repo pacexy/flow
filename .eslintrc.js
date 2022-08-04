69709089bf1274d8bd166f2bcfc56cb8d79143d8
pacexy <pacexy@gmail.com>
2022-08-04 23:25:19 +0800

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,34 @@
+const path = require('path')
+
 module.exports = {
-  extends: ['prettier'],
-  plugins: ['@typescript-eslint', 'import'],
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'next',
+  ],
+  settings: {
+    next: {
+      rootDir: path.join(__dirname, 'apps/reader'),
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
     'import/order': [
       'error',
       {
--- apps/reader/.eslintrc.js
+++ apps/reader/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  extends: ['../../.eslintrc.js', 'next'],
-  settings: {
-    next: {
-      rootDir: 'apps/reader',
-    },
-  },
+  extends: ['../../.eslintrc.js'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     '@next/next/no-img-element': 'off',
     'react/jsx-key': 'off',
     'react/no-children-prop': 'off',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useRecoilCallback|useRecoilTransaction_UNSTABLE',
+      },
+    ],
   },
 }
