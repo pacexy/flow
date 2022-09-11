9da44cc549e941cc5bd933568196123bc2d747ed
pacexy <pacexy@gmail.com>
2022-09-11 15:30:12 +0800

    feat: new layout

--- apps/reader/src/layout/Layout.tsx
+++ apps/reader/src/layout/Layout.tsx
@@ -12,20 +12,43 @@ export const Layout: React.FC = ({ children }) => {
 
 const Header: React.FC = () => {
   return (
-    <header className="typescale-body-large text-on-surface flex justify-center gap-8 py-2">
-      <Link href="/">Home</Link>
-      <Link href="/pricing">Pricing</Link>
+    <header className="typescale-body-large text-on-surface container flex h-12">
+      <a className="mr-8 flex items-center gap-4" href="/home">
+        <img src="icons/512.png" alt="Logo" className="w-8" />
+        <span className="typescale-headline-small">Lota</span>
+      </a>
+      <div className="text-outline typescale-body-large flex items-center gap-6">
+        <Link href="/pricing">Pricing</Link>
+        <Link href="/faq">FAQ</Link>
+      </div>
+      <a
+        className="typescale-title-medium ml-auto hidden bg-black px-4 py-3 text-white sm:block"
+        href="/"
+        target="_blank"
+      >
+        Open App
+      </a>
     </header>
   )
 }
 
 const Footer: React.FC = () => {
   return (
-    <footer className="typescale-body-medium text-outline mt-10 flex items-center justify-center gap-8 p-2 text-center">
-      <a href="mailto:pacexy@gmail.com">Contact</a>
-      <Link href="/terms">Terms of Use</Link>
-      <Link href="/privacy">Privacy Policy</Link>
-      <Link href="/refund">Refund Policy</Link>
+    <footer className="mt-10 bg-black py-4">
+      <div className="container">
+        <div className="text-inverse-on-surface typescale-body-small mb-4 flex gap-6">
+          <Link href="/terms">Terms</Link>
+          <Link href="/privacy">Privacy</Link>
+          <a href="mailto:pacexy@gmail.com">Contact</a>
+          <Link href="https://pacexy.notion.site/283696d0071c43bfb03652e8e5f47936?v=b43f4dd7a3cb4ce785d6c32b698a8ff5">
+            Roadmap
+          </Link>
+        </div>
+
+        <div className="typescale-body-small text-white">
+          © {new Date().getFullYear()} Lota
+        </div>
+      </div>
     </footer>
   )
 }
--- apps/reader/tailwind.config.js
+++ apps/reader/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {},
     container: {
       center: true,
+      padding: '2rem',
     },
   },
   plugins: [
