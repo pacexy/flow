951f97b125d785d16f312427ea6502b701e0c052
pacexy <pacexy@gmail.com>
2022-10-12 18:17:12 +0800

    fix: failed to `window.postMessage`

--- apps/reader/src/pages/_app.tsx
+++ apps/reader/src/pages/_app.tsx
@@ -8,7 +8,6 @@ import { UserProvider } from '@supabase/auth-helpers-react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import { RecoilRoot } from 'recoil'
-import { SWRDevTools } from 'swr-devtools'
 
 import { Layout } from '../components'
 
@@ -18,19 +17,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   if (router.pathname === '/success') return <Component {...pageProps} />
 
   return (
-    <SWRDevTools>
-      <ErrorBoundary fallback={<Fallback />}>
-        <UserProvider supabaseClient={supabaseClient}>
-          <LiteralProvider>
-            <RecoilRoot>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </RecoilRoot>
-          </LiteralProvider>
-        </UserProvider>
-      </ErrorBoundary>
-    </SWRDevTools>
+    <ErrorBoundary fallback={<Fallback />}>
+      <UserProvider supabaseClient={supabaseClient}>
+        <LiteralProvider>
+          <RecoilRoot>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </RecoilRoot>
+        </LiteralProvider>
+      </UserProvider>
+    </ErrorBoundary>
   )
 }
 
