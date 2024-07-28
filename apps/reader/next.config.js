f4983709a99a34965849f938dc526a3205124ffc
pacexy <pacexy@gmail.com>
2024-07-29 06:59:53 +0800

    Revert "rm sentry"
    This reverts commit 9bbe1bc0ce4f0a145bb928ed3aa58c2347767698.

--- apps/reader/next.config.js
+++ apps/reader/next.config.js
@@ -55,12 +55,11 @@ const base = withPWA(withTM(withBundleAnalyzer(config)))
 
 const dev = base
 const docker = base
-// const prod = withSentryConfig(
-//   base,
-//   // Make sure adding Sentry options is the last code to run before exporting, to
-//   // ensure that your source maps include changes from all other Webpack plugins
-//   sentryWebpackPluginOptions,
-// )
-const prod = base
+const prod = withSentryConfig(
+  base,
+  // Make sure adding Sentry options is the last code to run before exporting, to
+  // ensure that your source maps include changes from all other Webpack plugins
+  sentryWebpackPluginOptions,
+)
 
 module.exports = IS_DEV ? dev : IS_DOCKER ? docker : prod
