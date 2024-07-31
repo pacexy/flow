cc31872281772c582ee3899ac615c78e367be074
pacexy <pacexy@gmail.com>
2024-07-31 19:52:05 +0800

    format

--- apps/reader/src/hooks/useTouchEvent.ts
+++ apps/reader/src/hooks/useTouchEvent.ts
@@ -5,51 +5,12 @@ import { AsRef, BookTab } from '../models/reader'
 
 import { hasSelection } from './useTextSelection'
 
-export function useTouchEvent(props: { iframe?: Window & AsRef; tab: BookTab }) {
-  const { iframe, tab } = props;
+export function useTouchEvent(props: {
+  iframe?: Window & AsRef
+  tab: BookTab
+}) {
+  const { iframe, tab } = props
   const params = useRef({ x: -1, y: -1, t: 0, expired: false })
-
-  const handleTouchEnd = useCallback(function (e: TouchEvent) {
-    if (!iframe) return
-    iframe.ontouchend = undefined
-    console.log('params:', params.current)
-    const selection = iframe.getSelection()
-
-    if (hasSelection(selection)) return
-    if (params.current.expired) return
-
-    params.current.expired = true
-
-    const x1 = e.changedTouches[0]?.clientX ?? 0
-    const y1 = e.changedTouches[0]?.clientY ?? 0
-    const t1 = Date.now()
-
-    const { x, y, t } = params.current;
-
-    const deltaX = x1 - x
-    const deltaY = y1 - y
-    const deltaT = t1 - t
-
-    const absX = Math.abs(deltaX)
-    const absY = Math.abs(deltaY)
-
-    if (absX < 10) return
-
-    if (absY / absX > 2) {
-      if (deltaT > 100 || absX < 30) {
-        return
-      }
-    }
-
-    if (deltaX > 0) {
-      tab.prev()
-    }
-
-    if (deltaX < 0) {
-      tab.next()
-    }
-  }, [tab, iframe]);
-
 
   useEventListener(iframe, 'touchstart', (e) => {
     const x0 = e.targetTouches[0]?.clientX ?? 0
@@ -58,12 +19,57 @@ export function useTouchEvent(props: { iframe?: Window & AsRef; tab: BookTab }) 
 
     params.current = { x: x0, y: y0, t: t0, expired: false }
 
+    if (!iframe) return
+
     // When selecting text with long tap, `touchend` is not fired,
     // so instead of use `addEventlistener`, we should use `on*`
     // to remove the previous listener.
-    if (!iframe) return
     iframe.ontouchend = handleTouchEnd
-  });
+  })
+
+  const handleTouchEnd = useCallback(
+    function (e: TouchEvent) {
+      if (!iframe) return
+      iframe.ontouchend = undefined
+      console.log('params:', params.current)
+      const selection = iframe.getSelection()
+
+      if (hasSelection(selection)) return
+      if (params.current.expired) return
+
+      params.current.expired = true
+
+      const x1 = e.changedTouches[0]?.clientX ?? 0
+      const y1 = e.changedTouches[0]?.clientY ?? 0
+      const t1 = Date.now()
+
+      const { x, y, t } = params.current
+
+      const deltaX = x1 - x
+      const deltaY = y1 - y
+      const deltaT = t1 - t
+
+      const absX = Math.abs(deltaX)
+      const absY = Math.abs(deltaY)
+
+      if (absX < 10) return
+
+      if (absY / absX > 2) {
+        if (deltaT > 100 || absX < 30) {
+          return
+        }
+      }
+
+      if (deltaX > 0) {
+        tab.prev()
+      }
+
+      if (deltaX < 0) {
+        tab.next()
+      }
+    },
+    [tab, iframe],
+  )
 
   useEventListener(iframe, 'touchend', handleTouchEnd)
 }
