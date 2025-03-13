5ab36c219d39aa0bd24a0b68fe8799e955a743e0
jinjo <dlawlswh123@gmail.cllm>
2025-03-13 18:02:20 +0900

    fix: lint order error, 오타 수정

--- apps/reader/locales/index.ts
+++ apps/reader/locales/index.ts
@@ -1,12 +1,12 @@
 import en_US from '../locales/en-US'
-import ko_KR from '../locales/ko-KR'
 import ja_JP from '../locales/ja-JP'
+import ko_KR from '../locales/ko-KR'
 import zh_CN from '../locales/zh-CN'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   'en-US': en_US,
-  'ko-KR': ko_KR,
   'zh-CN': zh_CN,
+  'ko-KR': ko_KR,
   'ja-JP': ja_JP,
 } as const
